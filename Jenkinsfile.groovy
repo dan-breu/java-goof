969d757285cb4960f45f308937fb8c6692c81557
Danilo Briceño <68259418+dan-breu@users.noreply.github.com>
2023-08-08 23:44:06 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -24,7 +24,7 @@ pipeline {
                 snykSecurity(
         		snykInstallation: 'snyk@latest', snykTokenId: 'snyk-api-token',
 		    	severity: 'critical', failOnIssues: false,
-		    	additionalArguments: '--all-projects --detection-depth=4 -debug', 
+		    	additionalArguments: '--test --detection-depth=4 -debug', 
 			)
             }
         }
